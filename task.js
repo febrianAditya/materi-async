63256cf87d79987c4daeac30afbdd90b5be47c2b
FebrianAditya <febrian.aditya@student.upi.edu>
2023-09-15 17:34:37 +0700

    add soal

--- task.js
+++ task.js
@@ -1,70 +1,4 @@
 /**
- * setTimeout
- * 
- * Buatlah sebuah fungsi yang menampilkan pesan "Latihan dimulai dalam 5 detik" dan setelah menunggu selama 5 detik, menampilkan pesan "Mulai!".
- * 
- */
-
-mulaiHitungMundur();
-// Latihan dimulai dalam 5 detik...
-// Mulai!
-
-
-/**
- * setInterval
- * 
- * Buatlah sebuah fungsi yang akan menampilkan pesan "Detik ke-{n}" setiap detik. 
- * Fungsi ini harus berhenti menampilkan pesan setelah mencapai "Detik ke-5".
- * 
- */
-
-tampilkanDetik()
-// "Detik ke-1"
-// (setelah 1 detik) "Detik ke-2"
-// (setelah 1 detik lagi) "Detik ke-3"
-// (setelah 1 detik lagi) "Detik ke-4"
-// (setelah 1 detik lagi) "Detik ke-5"
-
-
-/**
- * Cek Lari (Gabungan setTimeout dan setInterval)
- * 
- * Buatlah sebuah fungsi yang ketika dijalankan akan menunggu selama 2 detik, kemudian menampilkan pesan "Mulai latihan!". 
- * Setelah itu, fungsi tersebut akan menampilkan pesan "Lari {n} meter!" setiap 1 detik. 
- * Fungsi ini harus berhenti setelah menampilkan pesan "Lari 10 meter!".
- * 
- */
-
-mulaiLatihan();
-// Tunggu 2 detik...
-// "Mulai latihan!"
-// (setelah 1 detik) "Lari 1 meter!"
-// (setelah 1 detik lagi) "Lari 2 meter!"
-// ... 
-// (setelah 1 detik lagi) "Lari 10 meter!"
-
-
-/**
- * Iterasi Array dengan callback
- * 
- * Buatlah fungsi iterasiArrayDenganCallback(arr, callback) yang menerima sebuah array dan sebuah callback. 
- * Fungsi tersebut harus memanggil callback untuk setiap elemen dalam array tersebut.
- */
-
-const data = [1, 2, 3, 4, 5];
-
-iterasiArrayDenganCallback([1, 2, 3], (item, index) => {
-    console.log(`Index: ${index}, Value: ${item}`);
-});
-// Output:
-// Index: 0, Value: 1
-// Index: 1, Value: 2
-// Index: 2, Value: 3
-// Index: 3, Value: 4
-// Index: 4, Value: 5
-
-
-/**
  * Latihan Callback dengan skenario belanja
  * 
  * Saldo awal sebesar Rp100.000. Anda ingin berbelanja di sebuah toko dengan urutan sebagai berikut:
@@ -82,26 +16,93 @@ iterasiArrayDenganCallback([1, 2, 3], (item, index) => {
  * 
  */
 
-function belanja() {
+let hargaBuku = 50000
+let barang1 = "Buku"
+
+let barang2 = "Pulpen"
+let hargaPulpen = 20000
+
+let barang3 = "Penghapus"
+let hargaPenghapus = 10000
+
+let barang4 = "Pensil"
+let hargaPensil = 5000
+// let functionCB = function (sisaSaldo);
+
+let saldoAwal = 100000
+
+function belanja(saldoAwal, hargaBarang, jenisBarang, callback) {
     
+    if (saldoAwal >= hargaBarang) {
+        saldoAwal -= hargaBarang
+        console.log(`Membeli ${jenisBarang} dengan harga ${hargaBarang}. Dan sisa saldo adalah ${saldoAwal}`);
+        callback(saldoAwal)
+    }else {
+        console.log("Duh uangnya ga cukup, silahkan nabung");
+    }
 }
+
+
+belanja(saldoAwal, hargaBuku, barang1, function(sisaSaldo) {
+
+    belanja(sisaSaldo, hargaPulpen, barang2, function(sisaSaldo) {
+
+        belanja(sisaSaldo, hargaPenghapus, barang3, function(sisaSaldo3) {
+
+            belanja(sisaSaldo3, hargaPensil, barang4, function (sisaSaldo4) {
+                
+                belanja(sisaSaldo4, hargaPenghapus, barang3, function(sisaSaldo5) {
+                    
+                    belanja(sisaSaldo5, hargaBuku, barang1, function(_) {
+                        
+                    })
+
+                })
+
+            })
+
+        })
+
+    })
+
+})
+
+
+
+
 //Output:
 // Membeli buku dengan harga Rp50.000. Sisa saldo: Rp50.000.
 // Membeli pulpen dengan harga Rp20.000. Sisa saldo: Rp30.000.
 // Membeli penghapus dengan harga Rp10.000. Sisa saldo: Rp20.000.
 // Membeli pensil dengan harga Rp5.000. Sisa saldo: Rp15.000.
 
+// ============================================================================================================================
+
+/**
+ * Buatlah tiga fungsi yang masing-masing mengembalikan Promise. 
+ * Fungsi pertama langkahPertama di-resolve DALAM 2 DETIK dengan pesan "Langkah 1 selesai", 
+ * fungsi kedua langkahKedua di-resolve DALAM 3 DETIK dengan pesan "Langkah 2 selesai", dan 
+ * fungsi ketiga langkahKetiga di-resolve DALAM 1 DETIK dengan pesan "Langkah 3 selesai".
+ * Panggil ketiga fungsi tersebut secara berurutan menggunakan chaining sehingga pesan ditampilkan dalam urutan yang benar.
+ */
+  
 
 
 /**
- * Pengurutan dengan Callback
- * 
- * Tuliskan sebuah fungsi urutkanArray(arr, compareFunc, callback) yang menerima sebuah array, fungsi perbandingan, dan sebuah callback. 
- * Fungsi tersebut harus mengembalikan array yang sudah diurutkan sesuai dengan fungsi perbandingan ke fungsi callback.
- * 
+ * Ubahlah chaining Promise dari soal sebelumnya menjadi sebuah fungsi async yang menggunakan await untuk menunggu setiap langkah selesai sebelum melanjutkan ke langkah berikutnya.
  */
 
-urutkanArray([3, 1, 4, 1], (a, b) => a - b, (hasil) => {
-    console.log(hasil); // [1, 1, 3, 4]
-});
-  
+
+
+/**
+ * Ubahlah Callback hell Belanja menjadi fungsi yang mengembalikan Promise
+ */
+
+
+
+/**
+ * Error Handling dengan Async/Await
+ * Buatlah sebuah fungsi async pembagian yang menerima dua argumen, pembilang dan penyebut. 
+ * Fungsi tersebut harus mengembalikan hasil bagi jika penyebut bukan 0 dan melempar sebuah kesalahan jika penyebut adalah 0. 
+ * Tangani kesalahan tersebut menggunakan blok try/catch dalam fungsi async.
+ */
